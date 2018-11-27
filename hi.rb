c89a0789df403ee261d48def2ebf05372f0c9b7c
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-27 22:04:10 +0000

    automatically backed up by learn

---
+++ hi.rb
@@ -0,0 +1,16 @@
+class Album
+  @@album_count = 0 
+ 
+  def initialize
+    @@album_count += 1
+  end
+ 
+  def self.count
+    @@album_count
+  end
+end
+Album.new
+Album.new
+Album.new
+
+Album.count
